bad009a18f73077d942a3e3a00c72352f182a3eb
Ihor <ihorrykun@gmail.com>
2023-03-18 22:13:31 +0200

    update

--- src/index.js
+++ src/index.js
@@ -5,8 +5,8 @@ import { BrowserRouter } from 'react-router-dom';
 import { App } from './App';
 import './index.css';
 import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
 import { persistor, store } from './redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -5,7 +5,10 @@ export function Home() {
     <section>
       <div>
         <h2>Phonebook manager welcome page</h2>
-        <ImgStyled src="./phonebook.png" alt="phone" />
+        <ImgStyled
+          src="https://png.pngtree.com/element_our/20190528/ourmid/pngtree-phone-book-icon-image_1130237.jpg"
+          alt="phone"
+        />
       </div>
     </section>
   );
